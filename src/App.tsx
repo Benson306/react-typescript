53b3db46fdd39b0585bffeb7159d85f51343aff6
Benson Ndiwa <bnkimtai@gmail.com>
2023-02-14 12:45:05 +0300

    UseState and Props in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputField from './Components/InputField';
 
 const App : React.FC = () => {
 
- 
+  const [todo, setTodo] = useState<string>('');
+
   return (
     <div className="App min-h-screen bg-slate-900">
       <h1 className='font-mono text-4xl p-8 text-white flex justify-center'>Taskify</h1>
 
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo}/>
+
     </div>
   );
 };
--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,10 +1,17 @@
-const InputField = () => {
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField = ({todo, setTodo}: Props) => {
     return ( <form  className="flex justify-center">
         <input 
             type="text" 
             name="" 
             placeholder="Enter a Task" 
             className=" p-3 w-4/6 rounded"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
             required
             id="" 
         />
