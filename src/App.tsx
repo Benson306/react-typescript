99368e2ef9d96bc591c1333ff6ea720a6d0ccfc1
Benson Ndiwa <bnkimtai@gmail.com>
2023-02-14 13:33:55 +0300

    Useref in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 import InputField from './Components/InputField';
+import { Todo } from './model';
 
 const App : React.FC = () => {
 
   const [todo, setTodo] = useState<string>('');
 
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) =>{
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, { id: Date.now() , todo: todo, isDone: false}])
+      setTodo("");
+    }
+    
+  }
+
+  console.log(todos)
+
   return (
     <div className="App min-h-screen bg-slate-900">
       <h1 className='font-mono text-4xl p-8 text-white flex justify-center'>Taskify</h1>
 
-      <InputField todo={todo} setTodo={setTodo}/>
-
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    {
+      todos.map( t => (
+        <div className='text-white p-4'>{t.todo}</div>
+      ))
+    }
     </div>
   );
 };
--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,11 +1,21 @@
+import { useRef } from "react";
+
 interface Props{
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({todo, setTodo}: Props) => {
-    return ( <form  className="flex justify-center">
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    return ( <form  className="flex justify-center" onSubmit={(e)=>{
+        handleAdd(e);
+        inputRef.current?.blur();
+        }}>
         <input 
+            ref={inputRef}
             type="text" 
             name="" 
             placeholder="Enter a Task" 
