1f697d502a8e77065289dcf38c9b2974cdb1601a
Benson Ndiwa <bnkimtai@gmail.com>
2023-02-14 14:41:16 +0300

    Edit Done

--- src/Components/SingleTodo.tsx
+++ src/Components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
@@ -11,6 +11,9 @@ type Props = {
 
 
 const SingleTodo = ({todo, todos, setTodos}:Props) => {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
 
     const handleDone = (id: number) => {
         setTodos(
@@ -28,16 +31,48 @@ const SingleTodo = ({todo, todos, setTodos}:Props) => {
         setTodos(todos.filter(todo => todo.id !== id))
     }
 
+    const handleEdit = (e: React.FormEvent, id:number) =>{
+        e.preventDefault();
+
+        setTodos(
+            todos.map(todo => {
+                if(todo.id === id) {
+                    return {...todo, todo:editTodo};
+                }else{
+                    return todo;
+                }
+            }) 
+        )
+        setEdit(false);
+    }
+
   return (
-    <form className='flex items-center bg-orange-400 p-5 m-1 gap-2'>
+    <form className='flex items-center bg-orange-400 p-5 m-1 gap-2' onSubmit={e => handleEdit(e,todo.id)}>
         {
-            todo.isDone ? (
-                <s className='w-4/6 font-serif text-lg'>{todo.todo}</s>
-            ) : (
-                <div className='w-4/6 font-serif text-lg'>{todo.todo}</div>
+            edit ? (
+               <input 
+                className='p-2' 
+                value={editTodo} 
+                onChange={(e)=> setEditTodo(e.target.value)}
+                name="" id="" 
+                />
+            ): (
+                
+                    todo.isDone ? (
+                        <s className='w-4/6 font-serif text-lg'>{todo.todo}</s>
+                    ) : (
+                        <div className='w-4/6 font-serif text-lg'>{todo.todo}</div>
+                    )
+                
             )
         }
-        <div className='w-1/6'><AiFillEdit /></div>
+        
+        <div className='w-1/6' onClick={()=>{
+            if(!edit && !todo.isDone){
+                setEdit(!edit);
+            }
+        }
+        }><AiFillEdit /></div>
         <div className='w-1/6' onClick={()=>handleDelete(todo.id)}><AiFillDelete /></div>
         <div className='w-1/6' onClick={()=>handleDone(todo.id)}><MdDone /></div>
 
