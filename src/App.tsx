7e715726bede7aada029c2e21eda4df8fd393b0e
Benson Ndiwa <bnkimtai@gmail.com>
2023-02-14 11:52:37 +0300

    Types

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,30 @@ function App() {
 
   let name: string = 'Benji';
 
+  let fruits: string[];
+
+  fruits = ['Lemon'];
+
+  //turple
+  let students: [string, number];
+  students = ['Benji',8];
+
+  //object
+  type Person = {
+    name: string,
+    age?: number //optional
+  }
+
+  let person : Person = {
+    name:'Benson',
+    age:22
+  }
+
+  //array of objects
+  let lostsOfPeople : Person[] ;
+
+  lostsOfPeople = [{name:'Benson'},{name: 'Oliver'}]
+
   return (
     <div className="App">
       {name}
